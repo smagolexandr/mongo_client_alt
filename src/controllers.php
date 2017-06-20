0f5e14c17811213b6a7e0bf718bf8c00b22b0f07
SmagOlexandr <smagolexandr@gmail.com>
2017-06-20 21:50:37 +0300

    add form and handler

--- src/controllers.php
+++ src/controllers.php
@@ -9,6 +9,8 @@ use Symfony\Component\HttpFoundation\Response;
 
 $app->match('/', function (Request $request) use ($app) {
 
+    $response = [];
+
     $data = array(
         'sql' => 'select * from people'
     );
@@ -20,29 +22,75 @@ $app->match('/', function (Request $request) use ($app) {
         ])
         ->getForm();
 
+    $response['sql'] = $form->createView();
+
     $form->handleRequest($request);
 
     if ($form->isValid()) {
         $data = $form->getData();
         if (isset($data['sql'])) {
 
+            if (strpos($data['sql'], "select") !== false) {
+
+                $q = strpos($data['sql'], "from");
+
+                if (trim(substr($data['sql'], 7, $q - 7)) == "") {
+                    return false;
+                }
+
+                if (trim(substr($data['sql'], 7, $q - 7)) == "*"){
+                    $projections = null;
+                } else {
+                    $projections = array_fill_keys(array_map('trim', explode(",", substr($data['sql'], 7, $q - 7))), 1) ;
+                    $projections["_id"] = 0;
+                }
+
+
+                if (strpos($data['sql'],"limit") !== null) {
+                    $limit_start = strpos($data['sql'],"limit") + 5;
+                    $limit_end = strpos($data['sql']," ", $limit_start+1);
+                    if ($limit_end == false ){
+                        $limit_end = strlen($data['sql']);
+                    }
+                    $limit = intval(trim(substr($data['sql'], $limit_start, $limit_end - $limit_start)));
+                }
+
+                if (strpos($data['sql'],"offset") !== null) {
+                    $limit_start = strpos($data['sql'],"offset") + 6;
+                    $limit_end = strpos($data['sql']," ", $limit_start+1);
+                    if ($limit_end == false ){
+                        $limit_end = strlen($data['sql']);
+                    }
+                    $offset = intval(trim(substr($data['sql'], $limit_start, $limit_end - $limit_start)));
+                }
+
+
+                $items = new ArrayObject();
+                $collection = $app['mongodb']->test->people;
+
+                $cursor = $collection->find(
+                    [
+//                        '$or' => [
+//                            ['name' => 'Olexandr'],
+//                            ['name' => 'Elizabeth'],
+//                        ],
+                    ],
+                    ['projection' => $projections,
+                        "limit" => $limit ? $limit : null,
+                        "skip" => $offset ? $offset : null,
+                        "sort" => ['age' => -1]
+                    ]
+                );
+
+                foreach ($cursor as $document) {
+                    $items->append($document);
+                }
+                dump($items);
+                $response['items'] = $items;
+            }
         }
-        dump($data);
     }
-
-
-    $people = new ArrayObject();
-    $collection = $app['mongodb']->test->people;
-
-    $cursor = $collection->find();
-
-    foreach ($cursor as $document) {
-        $people->append($document);
-    }
-
-//    dump($people);
-
-    return $app['twig']->render('index.html.twig', ["people" => $people, "sql" => $form->createView()]);
+    return $app['twig']->render('index.html.twig', $response);
 })
 ->method('GET|POST')
 ->bind('homepage')
